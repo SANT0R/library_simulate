0bcf62d75f41384894be86e0ea9396b48ddfc5f1
mehmetsantor <santorm555@gmail.com>
2020-08-27 19:29:51 +0300

    Update RentServiceImpl.java

--- src/main/java/com/santor/library_simulate/service/RentServiceImpl.java
+++ src/main/java/com/santor/library_simulate/service/RentServiceImpl.java
@@ -1,19 +1,13 @@
 package com.santor.library_simulate.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
-
-import com.santor.library_simulate.dto.ClientDTO;
+import com.santor.library_simulate.dao.RentRepository;
 import com.santor.library_simulate.dto.RentDTO;
-import com.santor.library_simulate.model.Client;
+import com.santor.library_simulate.mapper.RentMapper;
+import com.santor.library_simulate.model.Rent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.santor.library_simulate.model.Rent;
-import com.santor.library_simulate.dao.RentRepository;
-import com.santor.library_simulate.mapper.RentMapper;
+import java.util.List;
 
 @Service
 public class RentServiceImpl implements RentService {
