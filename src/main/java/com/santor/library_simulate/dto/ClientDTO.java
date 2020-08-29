65204fadee7abbd2630843b7bbdb836f9375cba9
mehmetsantor <santorm555@gmail.com>
2020-08-29 16:43:00 +0300

    Update ClientDTO.java

--- src/main/java/com/santor/library_simulate/dto/ClientDTO.java
+++ src/main/java/com/santor/library_simulate/dto/ClientDTO.java
@@ -1,12 +1,7 @@
 package com.santor.library_simulate.dto;
 
 import com.santor.library_simulate.dto.base.BaseDTO;
-import com.santor.library_simulate.model.Rent;
 import lombok.Data;
-
-import javax.persistence.OneToMany;
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 public class ClientDTO extends BaseDTO {
