c91d2590b879c353b5ada43b617f829de8a68e6a
mehmetsantor <santorm555@gmail.com>
2020-09-11 11:58:51 +0300

    Update RentServiceTest.java

--- src/test/java/com/santor/library_simulate/service/RentServiceTest.java
+++ src/test/java/com/santor/library_simulate/service/RentServiceTest.java
@@ -39,7 +39,6 @@ class RentServiceTest {
     @Test
     void addTest() {
 
-        rentService.deleteAll();
 
         final Book book = new Book();
         book.setReleaseYear(LocalDate.now());
@@ -52,29 +51,34 @@ class RentServiceTest {
         rent.getBooks().add(book);
 
 
+        rentService.deleteAll();
+
         rentRepository.save(rent);
 
-        int pageSum = 0;
-
-        for (Book book1 : rent.getBooks()) {
-            pageSum += book1.getPage();
-        }
-
-        int pageNumForADay = 5;
-
-        LocalDate expected1 = LocalDate.now().plusDays( pageSum / pageNumForADay );
         List<RentDTO> rents = rentService.getAll();
+
         for (RentDTO getRent : rents){
+
             if (getRent != null){
+
+                int pageSum = 0;
+
+                for (Book book1 : rent.getBooks()) {
+                    pageSum += book1.getPage();
+                }
+
+                int pageNumForADay = 5;
+
+                LocalDate expected1 = LocalDate.now().plusDays( pageSum / pageNumForADay );
 
                 LocalDate actual = getRent.getFinishDate();
                 assertEquals(actual , expected1);
 
 
-                Set<BookDTO> books = getRent.getBooks();
+                Set<BookDTO> booksDTO = getRent.getBooks();
                 int sumStock = 0;
 
-                for (BookDTO book1 : books){
+                for (BookDTO book1 : booksDTO){
                     sumStock+=book1.getStock();
 
                 }
